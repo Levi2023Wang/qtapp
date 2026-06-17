df174f82ca3ac42defb0fabe1997907fcaceb3f9
Levi <mailboxoflevi@gmail.com>
2026-06-17 20:53:33 +0800

    initial clean pyqt5

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import sys
-
-from PySide6.QtWidgets import QApplication
-from PySide6.QtWidgets import QWidget
-from PySide6.QtWidgets import QPushButton
-from PySide6.QtWidgets import QLabel
-from PySide6.QtWidgets import QVBoxLayout
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 
 app = QApplication(sys.argv)
 
 window = QWidget()
-window.setWindowTitle("我的第一个 PyQt")
+window.setWindowTitle("我的第一个 PyQt5")
 window.resize(400, 300)
 
 layout = QVBoxLayout()
 
-label = QLabel("欢迎来到 PyQt")
+label = QLabel("欢迎来到 PyQt5")
 
 button = QPushButton("点我")
 
 def click():
     label.setText("按钮被点击了")
-
 
 button.clicked.connect(click)
 
@@ -32,4 +25,4 @@ window.setLayout(layout)
 
 window.show()
 
-app.exec()
+sys.exit(app.exec_())
